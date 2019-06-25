ae782e0647fa60089c1a22b7c4b8b18b2e8bacf5
Deeparghya Dutta Barua <abaruaasia@gmail.com>
2019-06-25 21:24:07 +0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,3 +17,4 @@ from .gdrive_utils import get_gdrive_id
 from .gdrive_utils import ls_gdrive
 from .gdrive_utils import pull_from_gdrive
 from .gdrive_utils import push_to_gdrive
+from .gdrive_utils import push_to_tdrive
